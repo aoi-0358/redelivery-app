fc86075f086b1c936470982a4955cd4968883055
aoi-0358 <aogangcun@gmail.com>
2023-01-03 20:49:21 +0900

    エラー修正

--- src/components/AnswersList.jsx
+++ src/components/AnswersList.jsx
@@ -5,7 +5,7 @@ const AnswersList = (props) => {
   return (
     <div className="c-grid__answer">
       {props.answers.map((value, index) => {
-        return <Answer content={value.content} key={index.toStrings}/>
+        return <Answer content={value.content} key={index.toString()}/>
       })}
     
     </div>
