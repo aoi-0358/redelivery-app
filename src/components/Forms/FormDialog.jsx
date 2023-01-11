96b0ef23f8e202edb3c844a7e74847f55977c3ee
daiki-tadokoro <>
2023-01-12 08:50:12 +0900

    feat: モーダルに回答内容を反映する処理の追加

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -1,38 +1,39 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import TextInput from './TextInput'
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default class FormDialog extends React.Component {
   constructor(props) {
     super(props);
-
-} 
+  }
   render() {
     return (
       <Dialog
-      open={this.props.open}
-      onClose={this.props.handleClose}
-      aria-labelledby="alert-dialog-title"
-      aria-describedby="alert-dialog-description"
-    >
-      <DialogTitle id="alert-dialog-title">
-        この内容でお間違いなければ<br/>下の『依頼する』を選択してください
-      </DialogTitle>
-      <DialogContent>
-          <TextInput />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={this.props.handleClose}>依頼する</Button>
-        <Button onClick={this.props.handleClose} autoFocus>
-          戻る
-        </Button>
-      </DialogActions>
-    </Dialog>
-
-    )
+        open={this.props.open}
+        onClose={this.props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          この内容でお間違いなければ
+          <br />
+          下の『依頼する』を選択してください
+        </DialogTitle>
+        <DialogContent>
+          {this.props.selectedAnswers.map((seclectedAnswer) => {
+            return <p>{seclectedAnswer}</p>;
+          })}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.props.handleClose}>依頼する</Button>
+          <Button onClick={this.props.handleClose} autoFocus>
+            戻る
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
   }
 }
