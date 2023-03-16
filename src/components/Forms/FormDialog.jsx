155073f47929b67d18fa93ce9270339eb9f596dc
aoi-0358 <aogangcun@gmail.com>
2023-03-16 13:12:03 +0900

    🩹モーダルに選択した解答を表示させてmapメゾットで書き換えた。

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -6,6 +6,7 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
 function FormDialog(props) {
+  console.log(props);
   return (
     <Dialog
       open={props.open}
@@ -18,8 +19,14 @@ function FormDialog(props) {
         <br />
         下の『依頼する』を選択してください
       </DialogTitle>
-      {/* App.jsx７９[]で囲ってるから{props.selectedAnswers}をmapでまわすべき↓↓↓↓ */}
-      <DialogContent>{props.selectedAnswers}</DialogContent>
+      {/* App.jsx34 []で囲ってるから{props.selectedAnswers}をmapでまわす↓↓↓↓ */}
+      <DialogContent>
+        {props.selectedAnswers.map((selectedAnswers, index) => {
+        return (
+          <div key={index.toString()} className="selected-modal">{selectedAnswers}</div>
+          )
+        })}
+      </DialogContent>
       <DialogActions>
         <Button onClick={props.handleClose}>依頼する</Button>
         <Button onClick={props.handleClose}>戻る</Button>
