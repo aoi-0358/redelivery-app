526ecee4d40e99e079efc0caa20fe02ff4562ea0
aoi-0358 <aogangcun@gmail.com>
2023-03-16 19:17:19 +0900

    💪 map()メゾットと、その第一引数が理解したので、その解説。

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -19,12 +19,24 @@ function FormDialog(props) {
         <br />
         下の『依頼する』を選択してください
       </DialogTitle>
-      {/* App.jsx34 []で囲ってるから{props.selectedAnswers}をmapでまわす↓↓↓↓ */}
+      {/* App.jsx34 []で囲ってるから{props.selectedAnswers}をmapでまわしたほうがtextの修正があったときに一カ所だけで済む↓↓↓↓
+
+      　map(引数)で配列の各要素の一個ずつに処理が実行できる
+        今回選択された解答を取得するように実行したいから、第一引数にselectedAnswersをいれた
+        （このselectedAnswersはApp.jsxのuseStateからきたやつ）
+        そして、return内で{selectedAnswers}と書いて返すことで、選択された解答を表示することができた。
+      
+         昨晩の時点では、<DialogContent>に{props.selectedAnswers}を直接入れたので、選択された解答が各要素として認識されなかったので、改行されずに表示されていた。
+　　　　　なので<DialogContent>に{props.selectedAnswers.map((selectedAnswer) =>…とすることで選択された解答を順番に取得でき改行されて表示できた
+        */}
       <DialogContent>
-        {props.selectedAnswers.map((selectedAnswers, index) => {
-        return (
-          <div key={index.toString()} className="selected-modal">{selectedAnswers}</div>
-          )
+        {props.selectedAnswers.map((selectedAnswer, index) => {
+          return (
+            // 選択した解答を表示する処理
+            <div key={index.toString()} className="selected-modal">
+              {selectedAnswer}
+            </div>
+          );
         })}
       </DialogContent>
       <DialogActions>
